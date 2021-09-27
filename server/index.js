f78f69578ead34b76517ddce6eae89db0620e6ff
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2021-09-27 21:31:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
 // MangoDB connection setup
-const CONNECTION_URL = "mongodb+srv://testUser:testing123@cluster0.qg9w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://uusername:password@cluster0.qg9w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
