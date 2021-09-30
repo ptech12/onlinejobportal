84eb59b3d8e7065121236168b5d7ff7e29a66d00
Prasanth A R <raviprasanth45@gmail.com>
2021-09-30 21:33:22 +0530

    Backend part over, going for client side

--- server/index.js
+++ server/index.js
@@ -11,6 +11,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// making mail script
+app.post("/send", cors(), async (req, res) => {
+    let {text} = req.body;
+    let transport = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
+        }
+    });
+
+    await transport.sendMail(
+        {
+            from:process.env.MAIL_FROM,
+            to: "testip86@gmail.com",
+            subject: "Text Mail",
+            html:
+            `
+            <div className="email" style="
+            border: 1px solid black;
+            padding: 20px;
+            line-height: 2;
+            font-size: 20px;"> 
+            <h2> Here your mail!</h2>
+            <p>${text}</p>
+            </div>
+            `
+        }
+
+    );
+
+
+})
+
 // start up the server
 app.listen((process.env.PORT | 4000), ()=> {
     console.log("listening on 4000")
