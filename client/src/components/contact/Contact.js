a76d5a94c5bb909d2b08a2b825d1022f21e39b4d
Prasanth A R <raviprasanth45@gmail.com>
2021-10-02 15:03:44 +0530

    Axios for mail send

---
+++ client/src/components/contact/Contact.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import axios from "axios"
+
+export default function Contact() {
+    const [ sent, setSent ] = useState(false);
+    const [text, setText] = useState("")
+    const [name, setName] = useState("")
+    // Handle Send method
+    const handleSend = async() => {
+        setSent(true)
+        try {
+            await axios.post("http://localhost:4000/send", {
+                name,
+                text
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <div className="contact">
+           <form onSubmit={handleSend}>
+                <h3>Name</h3>
+                <input type="text" onChange={e => setName(e.target.value)} />
+                <br />
+                <h3>Message</h3>
+                <input type="textarea" id="2" value={text} onChange={(e) => setText(e.target.value)} />
+                <br />
+                <button type="submit" >Send</button>
+            </form>
+        </div>
+    )
+}
