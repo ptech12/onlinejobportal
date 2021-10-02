ecb92afec8e758d8ecb8dc8266f62d9d6fd914fc
Prasanth A R <raviprasanth45@gmail.com>
2021-10-02 15:04:14 +0530

    contact and ack mail made it, await testing.

--- server/index.js
+++ server/index.js
@@ -12,35 +12,71 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // making mail script
-app.post("/send", cors(), async (req, res) => {
-    let {text} = req.body;
+app.post("/send", cors(),  (req, res) => {
+    // let {text} = req.body;
+    console.log(req.body);
     let transport = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
+        host: "smtp.mailtrap.io",
         port: process.env.MAIL_PORT,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
         }
     });
-
-    await transport.sendMail(
-        {
-            from:process.env.MAIL_FROM,
-            to: "testip86@gmail.com",
+    const options = {
+            from: process.env.MAIL_FROM,
+            to: "test@test.com",
             subject: "Text Mail",
-            html:
-            `
+            html: `
             <div className="email" style="
             border: 1px solid black;
             padding: 20px;
             line-height: 2;
             font-size: 20px;"> 
             <h2> Here your mail!</h2>
-            <p>${text}</p>
+            <h4>${req.body.name}</h4>
+            <p>Message</p>
+            <br />
+            <p>${req.body.text}</p>
             </div>
             `
         }
-
+    transport.sendMail({
+        from:process.env.MAIL_FROM,
+        to: req.body.email,
+        subject: "Text Mail",
+        html: `
+        <div className="email" style="
+        border: 1px solid black;
+        padding: 20px;
+        line-height: 2;
+        font-size: 20px;"> 
+        <h2> Here your mail!</h2>
+        <h4>Your mail is recived</h4>
+        <p>Message</p>
+        <br />
+        <p>Thanks for contacting us!. We will reach you soon.</p>
+        </div>
+        `
+    }, (err, info) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Info :", info);
+            res.json({
+                message: "Email successfully sent"
+            });
+        }
+    )
+    transport.sendMail(options, (err, info) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Info :", info);
+            res.json({
+                message: "Email successfully sent"
+            });
+        }
     );
 
 
