4fd08848a575c0be6719186f8caa5e791928c9b0
Prasanth A R <raviprasanth45@gmail.com>
2021-10-02 15:13:41 +0530

    adding email input

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -5,12 +5,15 @@ export default function Contact() {
     const [ sent, setSent ] = useState(false);
     const [text, setText] = useState("")
     const [name, setName] = useState("")
+    const [mail, setMail] = useState("");
+    // const [name, setName] = useState("")
     // Handle Send method
     const handleSend = async() => {
         setSent(true)
         try {
             await axios.post("http://localhost:4000/send", {
                 name,
+                mail,
                 text
             })
         } catch (err) {
@@ -24,6 +27,8 @@ export default function Contact() {
                 <h3>Name</h3>
                 <input type="text" onChange={e => setName(e.target.value)} />
                 <br />
+                <h3>E-mail</h3>
+                <input type="email" onChange={e => setMail(e.target.value)}/>
                 <h3>Message</h3>
                 <input type="textarea" id="2" value={text} onChange={(e) => setText(e.target.value)} />
                 <br />
