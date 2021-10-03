7249f1ea662108f5be2d8c0a36c030b97e26f037
Prasanth A R <raviprasanth45@gmail.com>
2021-10-03 20:21:35 +0530

    Making form contact styling

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from "axios"
-
+import { Jumbotron, Row, Col, Container, Button, Form, FormGroup, Label, Input, FormText } from "reactstrap"
 export default function Contact() {
     const [ sent, setSent ] = useState(false);
     const [text, setText] = useState("")
@@ -23,18 +23,28 @@ export default function Contact() {
 
     return (
         <div className="contact">
-           <form onSubmit={handleSend}>
-                <h3>Name</h3>
-                <input type="text" onChange={e => setName(e.target.value)} />
-                <br />
-                <h3>E-mail</h3>
-                <input type="email" onChange={e => setMail(e.target.value)}/>
-                <h3>Message</h3>
-                <input type="textarea" id="2" value={text} onChange={(e) => setText(e.target.value)} />
-                <br />
-                <button type="submit" >Send</button>
-            </form>
-            {sent ? alert('fone') : console.log("Wait") }
+            <Container fluid>
+                <Jumbotron fluid>
+                    <Col xs="6">
+                        
+                    </Col>
+                </Jumbotron>        
+            </Container>
+            
         </div>
+        // <div className="contact">
+        //    <form onSubmit={handleSend}>
+        //         <h3>Name</h3>
+        //         <input type="text" onChange={e => setName(e.target.value)} />
+        //         <br />
+        //         <h3>E-mail</h3>
+        //         <input type="email" onChange={e => setMail(e.target.value)}/>
+        //         <h3>Message</h3>
+        //         <input type="textarea" id="2" value={text} onChange={(e) => setText(e.target.value)} />
+        //         <br />
+        //         <button type="submit" >Send</button>
+        //     </form>
+        //     {sent ? alert('fone') : console.log("Wait") }
+        // </div>
     )
 }
