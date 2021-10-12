8396f508d44700f84b1034c156e5f5b6c33a534e
Prasanth A R <raviprasanth45@gmail.com>
2021-10-12 15:39:02 +0530

    Back-end part over

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -7,4 +7,15 @@ router.get('/signup',(req,res)=>{
       res.send("hello");
 })
 
+// addinng post requst to this route
+router.post('/signup', (req, res)=>{
+    let {name, email, password} = req.body;
+    console.log(req.body);
+    if (!email || !password || !name)
+    {
+        return res.status(422).json({error: "Add all Data"})
+    }
+    return res.status(200).json({message: "DONE !"})
+})
+
 module.exports=router
