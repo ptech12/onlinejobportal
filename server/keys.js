4af8d16579777e13d52256c037756542c3306259
Prasanth A R <raviprasanth45@gmail.com>
2021-10-12 15:08:04 +0530

    using env pass

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
+const pass = process.env.DB_PASS
 module.exports = {
-    MONGO_URL: "mongodb+srv://testUser:prasanthsugu123@cluster0.qg9w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGO_URL: `mongodb+srv://testUser:${pass}@cluster0.qg9w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 }
