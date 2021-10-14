57b70e9bad55ffbcfbcd4d66a99fe430a367be1e
Prasanth A R <raviprasanth45@gmail.com>
2021-10-14 13:03:11 +0530

    Added local link need to added login/Register

--- client/src/components/Navbar/Navigation.js
+++ client/src/components/Navbar/Navigation.js
@@ -7,6 +7,22 @@ export default function Navigation() {
     const toggle = () => setIsOpen(!isOpen) 
 
     return (
-        <Navbar color="dark" dark expand="md"></Navbar>
+        <Navbar color="dark" dark expand="md">
+            <NavbarBrand href="/"> JobPortal </NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    <NavItem >
+                        <NavLink href="#abt-us">About Us </NavLink>
+                    </NavItem>
+                    <NavItem >
+                        <NavLink href="#srv">Service</NavLink>
+                    </NavItem>
+                    <NavItem >
+                        <NavLink href="#contact-id">Contact US</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
     );
 }
