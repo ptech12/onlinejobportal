feaa48c6d7dc7f2a0aa936235c9b47e7ccf068dc
Prasanth A R <raviprasanth45@gmail.com>
2021-10-15 16:26:19 +0530

    Version 1.0.0

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ mongoose.connection.on("connected", () => console.log("Connection success"))
 // on not successfull connection
 mongoose.connection.on("error", () => console.log("error!! ")  )
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello World")
+// })
 
 // Serve statics assests if it in production
 if ( process.env.NODE_ENV === 'production' )
