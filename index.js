a06c5e03f36fe66af332607f1e2e30f94f58e68f
Prasanth A R <raviprasanth45@gmail.com>
2021-10-16 10:55:27 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -1,122 +1,33 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
-const nodemailer = require('nodemailer');
 const mongoose = require("mongoose")
 const morgan = require('morgan');
-const {MONGO_URL} = require('./config/keys')
-require("./models/user")
-const mainRoutes = require("./Routes/routes")
 
-// USing Body Parser and cors
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.json());
-
-app.use(mainRoutes)
-// set .env to ./config/config.env
-
-require('dotenv').config({ path: "./config/config.env" });
-// connecting to mongoose
-mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+// configuration from .env to ./config/config.env
+require('dotenv').config({
+    path: "./config/config.env"
 })
 
-// on successfull connection
-mongoose.connection.on("connected", () => console.log("Connection success"))
+const app = express();
+// config for dev
+if (process.env.NODE_ENV === 'development')
+{
+    // CORS it will allow to deal with react for localhost at port 3000 without any problem
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }))
 
-// on not successfull connection
-mongoose.connection.on("error", () => console.log("error!! ")  )
+    // Morgan gives information abt of each request
+    app.use(morgan('dev'))
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World")
-// })
-
-
-// making mail script
-app.post("/send", cors(), async (req, res) => {
-    // let {text} = req.body;
-    console.log(req.body);
-    let transport = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: process.env.MAIL_PORT,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
-        }
-    });
-    const options = {
-            from: req.body.mail,
-            to: "test@test.com",
-            subject: "Text Mail",
-            html: `
-            <div className="email" style="
-            border: 1px solid black;
-            padding: 20px;
-            line-height: 2;
-            font-size: 20px;"> 
-            <h2> Here your mail!</h2>
-            <h4>${req.body.name}</h4>
-            <p>Message</p>
-            <br />
-            <p>${req.body.text}</p>
-            </div>
-            `
-        }
-   
-    await transport.sendMail(options, (err, info) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log("Info :", info);
-            res.json({
-                message: "Email successfully sent"
-            });
-        }
-    );
-
-    // Ack Mail Transporter
-    //  const transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: process.env.MAIL_FROM,
-    //         pass: process.env.MAIL_FROM_PASS
-    //     }
-    // }
-        
-    // )
-    // await transporter.sendMail({
-    //     from:process.env.MAIL_FROM,
-    //     to: req.body.email,
-    //     subject: "Text Mail",
-    //     html: `
-    //     <div className="email" style="
-    //     border: 1px solid black;
-    //     padding: 20px;
-    //     line-height: 2;
-    //     font-size: 20px;"> 
-    //     <h2> Here your mail!</h2>
-    //     <h4>Your mail is recived</h4>
-    //     <p>Message</p>
-    //     <br />
-    //     <p>Thanks for contacting us!. We will reach you soon.</p>
-    //     </div>
-    //     `
-    // }, (err, info) => {
-    //         if (err) {
-    //             console.log(err);
-    //         }
-    //         console.log("Info :", info);
-    //         res.json({
-    //             message: "Email successfully sent"
-    //         });
-    //     }
-    // )
-
+}
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Page Not FOUND"
+    })
 })
-
 // start up the server
 // server.listen(port, () => console.log("Server Running on " + port))
 const PORT = process.env.PORT;
